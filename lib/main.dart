6692b18f592bd2456aef05f70dce24b03a88a179
uzcoding <millioner.sm.8@gmail.com>
2021-04-06 22:39:07 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_ui/screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery UI',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrange,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
